5aeaa79dbe377e0dfea7e2453ab570d2b1b793d4
Dricc-new <diegoramoncc17@gmail.com>
2023-08-06 13:41:10 -0400

    createBooks route created

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -2,22 +2,19 @@ import { Book } from '../models/Book.js'
 // Get all books
 export async function indexBook(req, res) {
     const books = await Book.find()
-    res.send('dd')
-    // res.json(books)
+    res.json(books)
 }
 
 // Create a book
-export function storeBook(req, res) {
-    console.log(req.body)
-    res.send('asas')
-    // const { title, author, description } = req.body
-    // const newBook = new Book({
-    //     title: title,
-    //     author: author,
-    //     description: description,
-    // })
-    // console.log(newBook)
-    // res.send('hi')
+export async function storeBook(req, res) {
+    const { title, author, description } = req.body
+    const newBook = new Book({
+        title: title,
+        author: author,
+        description: description,
+    })
+    await newBook.save()
+    res.json(newBook)
 }
 
 // Get a book
