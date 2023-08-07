e03430ce005af3bb6f75661acf88a18c9c68573c
Dricc-new <diegoramoncc17@gmail.com>
2023-08-07 02:35:52 -0400

    commented controller

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -17,18 +17,26 @@ export async function storeBook(req, res) {
     const { title, author, description } = req.body
     let image
     if (req.files.image) {
+        // Upload image to cloudinary
         const result = await uploadImage(req.files.image.tempFilePath)
+
+        // Delete image from temporary file
         await fs.rm(req.files.image.tempFilePath)
+
+        // Set image data
         image = {
             url: result.secure_url,
             public_id: result.public_id
         }
     }
 
+    // Create new Book
     const newBook = new Book({
         title, author, description, image
     })
+
     try {
+        // Save book in database
         await newBook.save()
         res.json(newBook)
     } catch (err) {
@@ -39,7 +47,10 @@ export async function storeBook(req, res) {
 // Get a book
 export async function getBook(req, res) {
     try {
+        // Search for the book in the database.
         const book = await Book.findById(req.params.id)
+
+        // If it doesn't find it, it returns a 404 error.
         if (book) return res.sendStatus(404)
         res.json(book)
     } catch (err) {
@@ -51,6 +62,7 @@ export async function getBook(req, res) {
 export async function updateBook(req, res) {
     const { title, author, description } = req.body
     try {
+        // Update the book in database
         const book = await Book.findByIdAndUpdate(req.params.id, {
             title, author, description,
         })
@@ -63,9 +75,13 @@ export async function updateBook(req, res) {
 // Remove a Book
 export async function removeBook(req, res) {
     try {
+        // Delete the book from the database.
         const book = await Book.findByIdAndDelete(req.params.id)
+
+        // If it doesn't find it, it returns a 404 error.
         if (!book) return res.sendStatus(404)
         
+        // If you have an image, delete it from cloudinary
         if (book.image.public_id) await deleteImage(book.image.public_id)
         res.sendStatus(204)
     } catch (err) {
